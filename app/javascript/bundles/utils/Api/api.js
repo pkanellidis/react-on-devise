d35b850306408ee9ef7c7d66878151e599e69159
pkanellidis <panagiwthskan@gmail.com>
2024-10-19 02:17:40 +0300

    Implement API response helpers

---
+++ app/javascript/bundles/utils/Api/api.js
@@ -0,0 +1,16 @@
+
+export const readResponseErrors = (error) => {
+    let message
+
+    if (error.response) {
+        message = error.response.data.errors
+    } else {
+        message = "Something went wrong"
+    }
+
+    return message
+}
+
+export const readResponseMessage = (response) => {
+    return response.data && response.data.message
+}
